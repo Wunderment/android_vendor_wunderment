616fd1dc70505749817b923eda502e5289831858
Greg Ross <greg@toolstack.com>
2020-05-27 21:21:20 -0400

    Update common.mk

--- config/common.mk
+++ config/common.mk
@@ -7,6 +7,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     OpenWeatherProvider
 
+# Add Seedvault.
+PRODUCT_PACKAGES += \
+    Seedvault \
+    BasicDreams
+    
 # Remove the lineage signing keys from the recovery build, we only want ours here which get autoamtically
 # added during the signing process.
 PRODUCT_EXTRA_RECOVERY_KEYS := 
