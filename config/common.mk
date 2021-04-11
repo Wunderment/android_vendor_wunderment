bd196b7077c7efb42830253841a47bcd2ce8c783
Greg Ross <greg@toolstack.com>
2021-04-11 18:04:28 -0400

    Remove unused weather provider

--- config/common.mk
+++ config/common.mk
@@ -7,10 +7,6 @@ PRODUCT_PACKAGES += \
     F-DroidPrivilegedExtension \
     F-Droid
 
-# Add Lineage's OpenWeatherProvider.
-PRODUCT_PACKAGES += \
-    OpenWeatherProvider
-
 # Remove the lineage signing keys from the recovery build, we only want ours here which get autoamtically
 # added during the signing process.
 PRODUCT_EXTRA_RECOVERY_KEYS := 
